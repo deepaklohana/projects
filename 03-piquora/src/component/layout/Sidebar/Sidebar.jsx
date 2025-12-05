81c3f132699543afa13753b579c3f11af118c9a3
deepaklohana <deepaklohana41@gmail.com>
2025-12-05 18:48:17 +0500

    completed image slide and starts works on biodata scroll-bar

--- 03-piquora/src/component/layout/Sidebar/Sidebar.jsx
+++ 03-piquora/src/component/layout/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Sidebar = () => {
+    
   return (
     <div className='bg-white shadow-[0px_4px_34px_0px_rgba(0,0,0,0.05)] p-3 rounded-2xl w-[230px]'>
       sfgs
